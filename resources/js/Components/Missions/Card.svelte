3890b819513e0014e65f3b644c74856ce7c500f8
TomBurch <thomas.burch1306@gmail.com>
2022-08-23 00:33:14 +0100

    Rework card again

--- resources/js/Components/Missions/Card.svelte
+++ resources/js/Components/Missions/Card.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <!-- Adapted from https://flowbite.com/docs/components/card/#default-card -->
-<Link href="/missions/{mission.id}" class="rounded-lg border border-gray-700 shadow-md bg-green-800">
-    <img class="px-5" src="/images/arcomm-placeholder.jpg" alt="">
-    <div class="px-5 bg-gray-800">
-        <h5 class="truncate text-xl font-bold text-white tracking-tight">{mission.display_name}</h5>
-        <p class="truncate text-sm font-normal text-gray-400">By {mission.user.username}</p>
-        <p class="truncate text-sm font-normal text-gray-400">{mission.summary}</p>
+<Link href="/missions/{mission.id}" class="h-52 p-3 rounded-lg border border-gray-700 shadow-md bg-gray-800">
+    <img class="h-[50%] rounded-lg" src="/images/arcomm-placeholder.jpg" alt="">
+    <div class="pt-5">
+        <h5 class="truncate text-md font-semibold text-white tracking-tight">{mission.display_name}</h5>
+        <p class="truncate text-xs font-normal text-gray-400">By {mission.user.username}</p>
+        <p class="pb-3 truncate text-xs font-normal text-gray-400">{mission.summary}</p>
     </div>
 </Link>
