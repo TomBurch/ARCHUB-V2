5bc3993472691929c5db492917732ef786f25420
TomBurch <thomas.burch1306@gmail.com>
2022-08-28 00:38:56 +0100

    Make mission card image larger

--- resources/js/Components/Hub/Missions/Card.svelte
+++ resources/js/Components/Hub/Missions/Card.svelte
@@ -5,8 +5,8 @@
 
 <!-- Adapted from https://flowbite.com/docs/components/card/#default-card -->
 <Link href="/hub/missions/{mission.id}" class="h-52 p-3 rounded-lg border border-gray-700 shadow-md bg-gray-800">
-    <img class="h-[50%] rounded-lg" src="/images/arcomm-placeholder.jpg" alt="">
-    <div class="pt-5">
+    <div class="h-28 bg-center bg-[url('/images/arcomm-placeholder.jpg')]"></div>
+    <div class="pt-4">
         <h5 class="truncate text-md font-semibold text-white tracking-tight">{mission.display_name}</h5>
         <p class="truncate text-xs font-normal text-gray-400">By {mission.user.username}</p>
         <p class="pb-3 truncate text-xs font-normal text-gray-400">{mission.summary}</p>
