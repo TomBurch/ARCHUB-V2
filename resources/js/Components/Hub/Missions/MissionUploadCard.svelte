620903e8ad95a67ec2ed20023ac475cbca969f24
TomBurch <thomas.burch1306@gmail.com>
2022-08-27 19:46:23 +0100

    Add progress bar to file upload

--- resources/js/Components/Hub/Missions/MissionUploadCard.svelte
+++ resources/js/Components/Hub/Missions/MissionUploadCard.svelte
@@ -15,7 +15,11 @@
     <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-52 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col justify-center items-center pt-5 pb-6">
             <svg aria-hidden="true" class="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            {#if !$form.progress}
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            {:else}
+                <progress value={$form.progress.percentage} max="100">{$form.progress.percentage}%</progress>
+            {/if}
         </div>
         <form>
             <input id="dropzone-file" type="file" class="hidden" on:input={e => $form.mission = e.target.files[0]} on:change={submit}>
