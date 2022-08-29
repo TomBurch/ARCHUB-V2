6a7a504c44c11800e0285b9c78e71beae279c3b2
TomBurch <thomas.burch1306@gmail.com>
2022-08-29 22:45:09 +0100

    Add colour to mission cards

--- resources/js/Components/Hub/Missions/Card.svelte
+++ resources/js/Components/Hub/Missions/Card.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <!-- Adapted from https://flowbite.com/docs/components/card/#default-card -->
-<Link href="/hub/missions/{mission.id}" class="h-52 p-3 rounded-lg shadow-md border border-gray-700 bg-gray-800 hover:bg-gray-700">
-    <div class="h-28 bg-center bg-[url('/images/arcomm-placeholder.jpg')]"></div>
-    <div class="pt-4">
-        <h5 class="truncate text-md font-semibold text-white tracking-tight">{mission.display_name}</h5>
-        <p class="truncate text-xs font-normal text-gray-400">By {mission.user.username}</p>
-        <p class="pb-3 truncate text-xs font-normal text-gray-400">{mission.summary}</p>
+<Link href="/hub/missions/{mission.id}" class="space-y-1 h-60 p-3 rounded-lg shadow-md border-b-8 border-b-rose-500 bg-gray-800 hover:bg-gray-700">
+    <div class="h-32 rounded-lg bg-center bg-[url('/images/arcomm-placeholder.jpg')]"></div>
+    <div class="space-y-0.5">
+        <h5 class="truncate text-md font-semibold text-rose-500 tracking-tight">{mission.display_name}</h5>
+        <p class="truncate text-xs font-normal text-gray-100">By {mission.user.username}</p>
+        <p class="truncate text-xs font-normal text-gray-100">{mission.summary}</p>
     </div>
 </Link>
