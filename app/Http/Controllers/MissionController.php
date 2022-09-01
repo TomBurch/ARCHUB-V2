556e691e24e517920948efcd5469a24fcbc21b51
TomBurch <thomas.burch1306@gmail.com>
2022-09-01 21:35:19 +0100

    Decode with PboMission

--- app/Http/Controllers/MissionController.php
+++ app/Http/Controllers/MissionController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\PBOMission\PBOMission;
 use App\Models\Mission;
-use Faker\Factory;
 
+use \stdClass;
+use Faker\Factory;
 use Illuminate\Http\Request;
 
 class MissionController extends Controller
@@ -20,14 +22,47 @@ class MissionController extends Controller
     public function store(Request $request)
     {
         $missionFile = $request->file('mission');
+        $fileName = $missionFile->getClientOriginalName();
         $user = auth()->user();
 
-        $faker = Factory::create();
+        // Temporarily store locally
+        $path = $missionFile->storeAs(
+            "missions/{$user->id}",
+            $fileName
+        );
+
+        $contents = $this->getMissionContents($path);
+        $details = $this->getDetailsFromFileName($fileName);
+        
         $mission = Mission::create([
             'user_id' => $user->id,
-            'display_name' => $missionFile->getClientOriginalName(),
-            'mode' => 'coop',
-            'summary' => $faker->catchPhrase(),
+            'display_name' => $contents['mission']['name'],
+            'mode' => $details->mode,
+            'summary' => $contents['mission']['description'],
         ]);
+    }
+
+    public function getMissionContents(string $path) {
+        $mission = new PBOMission(storage_path("app/{$path}"));
+        $contents = $mission->export();
+
+        // if ($mission->error) {
+        //     return new Exception($mission->errorReason);
+        // }
+
+        return $contents;
+    }
+
+    public function getDetailsFromFileName($name) {
+        $name = substr($name, 0, -4);
+        $mapName = last(explode('.', $name));
+        $parts = explode('_', rtrim($name, ".{$mapName}"));
+        $mode = strtolower($parts[1]);
+
+        $details = new stdClass();
+        $details->mode = $mode;
+        $details->map = $mapName;
+
+        return $details;
     }
 }
