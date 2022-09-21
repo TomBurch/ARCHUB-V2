b6bde30d0a05f895006e329bc5405673fa314776
TomBurch <thomas.burch1306@gmail.com>
2022-09-21 22:49:33 +0100

    Move verify button to other side

--- resources/js/Pages/Hub/Missions/Mission.svelte
+++ resources/js/Pages/Hub/Missions/Mission.svelte
@@ -31,9 +31,6 @@
         <p class="pt-2 truncate text-center text-sm font-normal text-gray-300">{mission.summary}</p>
     </div>
     <div class="flex gap-3 pt-1">
-        {#if can.verify_missions}
-            <MissionVerifyButton mission={mission}/>
-        {/if}
         {#if can.test_mission}
             <MissionUpdateButton mission={mission}/>
             <a href="/hub/missions/{mission.id}/download">
@@ -41,6 +38,9 @@
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                 </svg>
             </a>
+        {/if}
+        {#if can.verify_missions}
+            <MissionVerifyButton mission={mission}/>
         {/if}
     </div>
     <div class="pt-3">
