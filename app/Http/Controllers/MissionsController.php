d7ddbf6402206b20ea2f40b9c0c59b18ed71913e
TomBurch <thomas.burch1306@gmail.com>
2022-09-24 23:00:04 +0100

    Split missions into played/unplayed

--- app/Http/Controllers/MissionsController.php
+++ app/Http/Controllers/MissionsController.php
@@ -12,13 +12,10 @@ class MissionsController extends Controller
     public function index()
     {
         $missions = Mission::with('user:id,username')
-            ->select('id', 'user_id', 'display_name', 'mode', 'summary', 'verified_by')
-            ->orderBy('id', 'desc')->get()->toArray();
-
-        $user_id = auth()->user()->id;
-        $my_missions = array_filter($missions, function ($v) use ($user_id) {
-            return $v['user_id'] == $user_id;
-        });
+            ->select('id', 'user_id', 'display_name', 'mode', 'summary', 'verified_by', 'last_played')
+            ->orderBy('last_played', 'desc')
+            ->orderBy('id', 'desc')
+            ->get()->toArray();
 
         $next_operation = Operation::with([
             'missions:operation_id,mission_id' => [
@@ -32,7 +29,6 @@ class MissionsController extends Controller
 
         return inertia('Hub/Missions/Missions', [
             'missions' => $missions,
-            'my_missions' => $my_missions,
             'next_operation' => $next_operation,
             'can' => [
                 'test_missions' => Gate::allows('test-missions'),
--- resources/js/Pages/Hub/Missions/Missions.svelte
+++ resources/js/Pages/Hub/Missions/Missions.svelte
@@ -4,16 +4,15 @@
 </script>
 
 <script lang="ts">
+    import { page } from "@inertiajs/inertia-svelte";
     import Card from "../../../Components/Hub/Missions/Card.svelte";
     import MissionUploadCard from "../../../Components/Hub/Missions/MissionUploadCard.svelte";
 
     export let missions;
-    export let my_missions;
     export let next_operation;
     export let can;
 
     missions = Object.values(missions);
-    my_missions = Object.values(my_missions);
 </script>
 
 <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
@@ -32,16 +31,28 @@
         class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
     >
         <MissionUploadCard />
-        {#each my_missions as mission}
-            <Card {mission} />
+        {#each missions as mission}
+            {#if mission.user.id == $page.props.auth.user.id}
+                <Card {mission} />
+            {/if}
         {/each}
     </div>
-    <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">All missions</h3>
+    <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">Unplayed missions</h3>
     <div
         class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
     >
         {#each missions as mission}
-            {#if mission.verified_by || can.test_missions}
+            {#if !mission.last_played && (mission.verified_by || can.test_missions)}
+                <Card {mission} />
+            {/if}
+        {/each}
+    </div>
+    <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">Played missions</h3>
+    <div
+        class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+    >
+        {#each missions as mission}
+            {#if mission.last_played && (mission.verified_by || can.test_missions)}
                 <Card {mission} />
             {/if}
         {/each}
