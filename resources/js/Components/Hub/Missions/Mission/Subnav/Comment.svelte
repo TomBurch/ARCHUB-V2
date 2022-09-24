3c83429951fb0bad47ea4745da878da64b3726af
TomBurch <thomas.burch1306@gmail.com>
2022-09-24 14:02:57 +0100

    Fix comment width expanding out container

--- resources/js/Components/Hub/Missions/Mission/Subnav/Comment.svelte
+++ resources/js/Components/Hub/Missions/Mission/Subnav/Comment.svelte
@@ -27,12 +27,14 @@
 
 <div class="flex py-4 text-gray-200">
     <img class="mr-2 mt-1 h-8 w-8 rounded-full" src={comment.user.avatar} alt="" />
-    <div>
+    <div class="min-w-0 break-words">
         <div class="rounded-3xl bg-gray-700 px-4 py-2">
             <div class="text-sm font-semibold leading-relaxed">
                 {comment.user.username}
             </div>
-            <SvelteMarkdown source={comment.text} {options} />
+            <div>
+                <SvelteMarkdown source={comment.text} {options} />
+            </div>
         </div>
         <div class="mt-1 flex space-x-2">
             <div class="ml-4 text-sm text-gray-400">14 w</div>
