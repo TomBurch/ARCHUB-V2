aaf0025f9aae2a6b8200a57975e575c85073ba53
TomBurch <thomas.burch1306@gmail.com>
2022-09-26 19:13:29 +0100

    Add MissionCollection

---
+++ resources/js/Components/Hub/Missions/Mission/MissionCollection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Card from "../Card.svelte";
+    import MissionUploadCard from "../MissionUploadCard.svelte";
+
+    export let title;
+    export let missions;
+    export let uploadCard = false;
+</script>
+
+<h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">{title}</h3>
+<div
+    class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+>
+    {#if uploadCard}
+        <MissionUploadCard />
+    {/if}
+    {#each missions as mission}
+        <Card {mission} />
+    {/each}
+</div>
--- resources/js/Pages/Hub/Missions/Missions.svelte
+++ resources/js/Pages/Hub/Missions/Missions.svelte
@@ -5,56 +5,25 @@
 
 <script lang="ts">
     import { page } from "@inertiajs/inertia-svelte";
-    import Card from "../../../Components/Hub/Missions/Card.svelte";
-    import MissionUploadCard from "../../../Components/Hub/Missions/MissionUploadCard.svelte";
+    import MissionCollection from "../../../Components/Hub/Missions/Mission/MissionCollection.svelte";
 
     export let missions;
     export let next_operation;
     export let can;
 
-    missions = Object.values(missions);
+    $: next_op_missions = next_operation.missions.map((mission) => mission.mission);
+    $: my_missions = missions.filter((mission) => mission.user.id == $page.props.auth.user.id);
+    $: unplayed_missions = missions.filter(
+        (mission) => !mission.last_played && (mission.verified_by || can.test_missions)
+    );
+    $: played_missions = missions.filter(
+        (mission) => mission.last_played && (mission.verified_by || can.test_missions)
+    );
 </script>
 
 <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-    {#if next_operation}
-        <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">Next operation</h3>
-        <div
-            class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-        >
-            {#each next_operation.missions as mission}
-                <Card mission={mission.mission} />
-            {/each}
-        </div>
-    {/if}
-    <h3 class="mb-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">My missions</h3>
-    <div
-        class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-    >
-        <MissionUploadCard />
-        {#each missions as mission}
-            {#if mission.user.id == $page.props.auth.user.id}
-                <Card {mission} />
-            {/if}
-        {/each}
-    </div>
-    <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">Unplayed missions</h3>
-    <div
-        class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-    >
-        {#each missions as mission}
-            {#if !mission.last_played && (mission.verified_by || can.test_missions)}
-                <Card {mission} />
-            {/if}
-        {/each}
-    </div>
-    <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">Played missions</h3>
-    <div
-        class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
-    >
-        {#each missions as mission}
-            {#if mission.last_played && (mission.verified_by || can.test_missions)}
-                <Card {mission} />
-            {/if}
-        {/each}
-    </div>
+    <MissionCollection title={"Next operation"} missions={next_op_missions} />
+    <MissionCollection title={"My missions"} missions={my_missions} uploadCard={true} />
+    <MissionCollection title={"Unplayed missions"} missions={unplayed_missions} />
+    <MissionCollection title={"Played missions"} missions={played_missions} />
 </div>
