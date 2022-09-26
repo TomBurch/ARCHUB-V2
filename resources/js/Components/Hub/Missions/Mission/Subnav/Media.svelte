8c216bf19a5a58d04447cb65a18382970d989796
TomBurch <thomas.burch1306@gmail.com>
2022-09-26 21:45:56 +0100

    Remove mediauploadcard

--- resources/js/Components/Hub/Missions/Mission/Subnav/Media.svelte
+++ resources/js/Components/Hub/Missions/Mission/Subnav/Media.svelte
@@ -5,7 +5,6 @@
 </script>
 
 <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-    <MediaUploadCard {mission} />
     {#each mission.media as media}
         <img src={media} alt="thumb" />
     {/each}
