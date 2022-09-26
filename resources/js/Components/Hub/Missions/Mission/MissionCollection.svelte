65e961249cde4e034b5c8b57a92139cc33663193
TomBurch <thomas.burch1306@gmail.com>
2022-09-26 19:35:28 +0100

    Add show/hide button to mission collections

--- resources/js/Components/Hub/Missions/Mission/MissionCollection.svelte
+++ resources/js/Components/Hub/Missions/Mission/MissionCollection.svelte
@@ -5,16 +5,38 @@
     export let title;
     export let missions;
     export let uploadCard = false;
+
+    let open = true;
 </script>
 
-<h3 class="my-2 text-center text-xl font-bold uppercase leading-tight text-blue-600">{title}</h3>
-<div
-    class="col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
->
-    {#if uploadCard}
-        <MissionUploadCard />
-    {/if}
-    {#each missions as mission}
-        <Card {mission} />
-    {/each}
+<div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+    <button on:click={(e) => (open = !open)} class="flex justify-center text-blue-600">
+        <h3 class="my-2 text-center text-xl font-bold uppercase leading-tight">{title}</h3>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2.5"
+            stroke="currentColor"
+            class="ml-1 mt-2.5 h-6 w-6"
+        >
+            {#if open}
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            {:else}
+                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+            {/if}
+        </svg>
+    </button>
+    <div
+        class="{open
+            ? ''
+            : 'hidden'} col-span-full grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6"
+    >
+        {#if uploadCard}
+            <MissionUploadCard />
+        {/if}
+        {#each missions as mission}
+            <Card {mission} />
+        {/each}
+    </div>
 </div>
--- resources/js/Pages/Hub/Missions/Missions.svelte
+++ resources/js/Pages/Hub/Missions/Missions.svelte
@@ -21,7 +21,7 @@
     );
 </script>
 
-<div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+<div>
     <MissionCollection title={"Next operation"} missions={next_op_missions} />
     <MissionCollection title={"My missions"} missions={my_missions} uploadCard={true} />
     <MissionCollection title={"Unplayed missions"} missions={unplayed_missions} />
