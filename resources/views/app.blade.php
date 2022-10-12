9a76512e3de58c7cdad24dac768dafe1af51d630
TomBurch <thomas.burch1306@gmail.com>
2022-10-12 23:15:43 +0100

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
 
-    @vite
+    @vite(['resources/scripts/main.ts'])
 
     <!-- @production
         @vite(['resources/js/main.ts'])
