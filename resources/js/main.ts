114f15f9f50c687793e7b58d3f1ad02599df3f1e
TomBurch <thomas.burch1306@gmail.com>
2022-10-12 21:38:43 +0100

    Update dependencies, rewrite inertia setup

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,22 +1,24 @@
 import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/inertia-svelte'
 
-export const withVite = (name) => {
-  const pages = import.meta.glob("./Pages/**/*.svelte");
+/**
+ * Imports the given page component from the page record.
+ */
+function resolvePageComponent(name: string, pages: Record<string, any>) {
   for (const path in pages) {
-    if (path.endsWith(`${name.replace(".", "/")}.svelte`)) {
-      return typeof pages[path] === "function" ? pages[path]() : pages[path];
+    if (path.endsWith(`${name.replace('.', '/')}.svelte`)) {
+      return typeof pages[path] === 'function'
+        ? pages[path]()
+        : pages[path]
     }
   }
 
-  throw new Error(`Page not found: ${name}`);
-};
+  throw new Error(`Page not found: ${name}`)
+}
 
 createInertiaApp({
-  resolve: (name) => {
-    return withVite(name);
-  },
+  resolve: name => resolvePageComponent(name, import.meta.glob('../js/Pages/**/*.svelte')),
   setup({ el, App, props }) {
-    new App({ target: el, props });
+    new App({ target: el, props })
   },
-});
+})
