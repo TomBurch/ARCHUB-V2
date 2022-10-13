4f19cfb6d4a86103466a8c5ee0c92df085f496c3
TomBurch <thomas.burch1306@gmail.com>
2022-10-13 23:15:03 +0100

    Change outDir

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig(({ command }) => {
     return {
         build: {
-            outDir: "public/build",
             manifest: true,
             rollupOptions: {
                 input: ["resources/scripts/main.ts"]
